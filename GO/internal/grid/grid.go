33f27137f542be8f6a80d94c8043fa15fb2b3827
ZzDangprozZ3 <lehongdang2k4@gmail.com>
2025-01-17 15:08:17 +0100

    Average225

--- GO/internal/grid/grid.go
+++ GO/internal/grid/grid.go
@@ -44,47 +44,17 @@ func Average9(before [][]color.Color) (after [][]color.Color) {
 			for k := -7; k < 8; k++ { //Calculer la somme de r g b a de tous les 9 pixels voisins
 				for f := -7; f < 8; f++ {
 					r1, g1, b1, a1 := before[i+k][j+f].RGBA()
-					r = r + r1/257/2
-					g = g + g1/257/2
-					b = b + b1/257*0
+					r = r + r1/257
+					g = g + g1/257
+					b = b + b1/257
 					a = a + a1/257
 
 				}
 			}
-			var r_moyen uint8 = uint8(r)
-			var g_moyen uint8 = uint8(g)
-			var b_moyen uint8 = uint8(b)
-			var a_moyen uint8 = uint8(a)
-			pix := color.NRGBA{r_moyen, g_moyen, b_moyen, a_moyen}
-			y = append(y, pix)
-		}
-		after = append(after, y)
-	}
-	return
-}
-func Average9(before [][]color.Color) (after [][]color.Color) {
-
-	for i := 1; i < len(before)-1; i++ {
-		var y []color.Color
-		for j := 1; j < len(before[0])-1; j++ { //parcourir tous les pixels de l'image
-			var r uint32 = 0
-			var g uint32 = 0
-			var b uint32 = 0
-			var a uint32 = 0
-			for k := -1; k < 2; k++ { //Calculer la somme de r g b a de tous les 9 pixels voisins
-				for f := -1; f < 2; f++ {
-					r1, g1, b1, a1 := before[i+k][j+f].RGBA()
-					r = r + r1
-					g = g + g1
-					b = b + b1
-					a = a + a1
-
-				}
-			}
-			var r_moyen uint8 = uint8(r / 9)
-			var g_moyen uint8 = uint8(g / 9)
-			var b_moyen uint8 = uint8(b / 9)
-			var a_moyen uint8 = uint8(a / 9)
+			var r_moyen uint8 = uint8(r / 225)
+			var g_moyen uint8 = uint8(g / 225)
+			var b_moyen uint8 = uint8(b / 225)
+			var a_moyen uint8 = uint8(a / 225)
 			pix := color.NRGBA{r_moyen, g_moyen, b_moyen, a_moyen}
 			y = append(y, pix)
 		}
