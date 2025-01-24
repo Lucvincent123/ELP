1aec63c1cbc8b15597bc3dd5de9d213e780c927a
ThienVo <votrungthien1503@gmail.com>
2025-01-24 22:59:11 +0100

    graceful shutdown

--- GO/internal/tcp/server.go
+++ GO/internal/tcp/server.go
@@ -5,27 +5,26 @@ import (
 	"GO/internal/ioFile"
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"sync"
 	"time"
 )
 
-var wg sync.WaitGroup
-var mutex sync.Mutex
-
 type Server struct {
 	listenAddress string
 	listener      net.Listener
-	quit          chan bool
+	quit          chan struct{}
 	conns         []*net.Conn
-	quitConns     []chan bool
+	wg            sync.WaitGroup
+	mutex         sync.Mutex
 }
 
 func NewServer(listenAddress string) *Server {
 	return &Server{
 		listenAddress: listenAddress,
-		quit:          make(chan bool),
+		quit:          make(chan struct{}),
 	}
 }
 
@@ -35,74 +34,76 @@ func (s *Server) Start() {
 		fmt.Println("Cannot open tcp:", err)
 		return
 	}
-	defer listener.Close()
+	// defer listener.Close()
 	s.listener = listener
 	fmt.Println("Listening at", s.listener.Addr().String())
+	s.wg.Add(1)
 	go s.acceptLoop()
 	s.commandLoop()
-	fmt.Println("Closing server...")
-	wg.Wait()
-	close(s.quit)
+	// close(s.quit)
+	s.wg.Wait()
+	listener.Close()
 	fmt.Println("Closed")
 }
 
 func (s *Server) acceptLoop() {
+	defer s.wg.Done()
+	for {
+		fmt.Println("acceptLoop")
+		conn, err := s.listener.Accept()
+		if err != nil {
+			select {
+			case <-s.quit:
+				fmt.Println("quit")
+				return
+			default:
+				fmt.Println("Cannot accept connection:", err)
+			}
+
+		} else {
+			fmt.Println("\nnew connection", conn.RemoteAddr())
+			fmt.Printf("(server%v)->", s.listenAddress)
+			s.mutex.Lock()
+			s.conns = append(s.conns, &conn)
+			s.mutex.Unlock()
+			go func() {
+				s.readLoop(conn)
+			}()
+		}
+
+	}
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+	defer fmt.Println(conn.RemoteAddr(), "is closed")
+	defer conn.Close()
+	// buffer := make([]byte, 10000000000)
+	fmt.Println("reading loop")
+	reader := bufio.NewReader(conn)
 	for {
 		select {
 		case <-s.quit:
 			fmt.Println("quit")
 			return
 		default:
-			fmt.Println("acceptLoop")
-			conn, err := s.listener.Accept()
-			if err != nil {
-				fmt.Println("Cannot accept connection:", err)
-				return
-			}
-			quitConn := make(chan bool)
-			mutex.Lock()
-			s.conns = append(s.conns, &conn)
-			s.quitConns = append(s.quitConns, quitConn)
-			mutex.Unlock()
-			fmt.Println("\nnew connection", conn.RemoteAddr())
-			fmt.Printf("(server%v)->", s.listenAddress)
-			fmt.Println(s.quitConns)
-			fmt.Println(s.conns)
-			wg.Add(1)
-			go s.readLoop(conn, quitConn)
-		}
-	}
-}
-
-func (s *Server) readLoop(conn net.Conn, quitConn chan bool) {
-	defer conn.Close()
-	defer wg.Done()
-	buffer := make([]byte, 10000000000)
-	for {
-		select {
-		case <-quitConn:
-			fmt.Println(conn.RemoteAddr(), "closed")
-			return
-		default:
-			fmt.Println("reading loop")
-			n, err := conn.Read(buffer)
-			if err != nil {
-				fmt.Println("Cannot read:", err)
+			buffer, err := reader.ReadBytes(byte(2))
+			if err != nil && err != io.EOF {
+				fmt.Println("Cannot read", err)
 				return
 			}
 			if buffer[0] == byte(1) {
 				if buffer[1] == byte(0) {
-					wg.Add(1)
+					s.wg.Add(1)
 					go func() {
-						defer wg.Done()
+						defer s.wg.Done()
 						var width int = int(buffer[2])*255 + int(buffer[3])
-						imgGrid := ioFile.BytesToGrid(buffer[4:n], width)
+						imgGrid := ioFile.BytesToGrid(buffer[4:], width)
 
 						imgFiltered := grid.Average(imgGrid, 5)
-						mutex.Lock()
+						s.mutex.Lock()
 						ioFile.Save("server.png", imgFiltered)
 						fmt.Printf("(server%v)->", s.listenAddress)
-						mutex.Unlock()
+						s.mutex.Unlock()
 						pix, newWidth := ioFile.GridToBytes(imgFiltered)
 						var data []byte
 						data = append(data, byte(1))
@@ -114,18 +115,20 @@ func (s *Server) readLoop(conn net.Conn, quitConn chan bool) {
 						conn.Write(data)
 					}()
 				}
-			} else {
-				msg := string(buffer[:n])
-				if msg == "close" {
-					conn.Write([]byte("close"))
-					close(quitConn)
-				}
+			}
+			msg := string(buffer)
+			if msg == "close" {
+				conn.Write([]byte("close"))
+				// close(s.quit)
 			}
 		}
+		// n, err := conn.Read(buffer)
+
 	}
 }
 
 func (s *Server) commandLoop() {
+	// defer s.wg.Done()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Printf("(server%v)->", s.listenAddress)
@@ -133,8 +136,10 @@ func (s *Server) commandLoop() {
 			cmd := scanner.Text() // Read command string
 			if cmd == "exit" {
 				fmt.Print("exit is press")
-				s.stop()
-				break
+				// s.Stop()
+				close(s.quit)
+				fmt.Println("Closing server...")
+				return
 			}
 		}
 		if err := scanner.Err(); err != nil {
@@ -143,15 +148,16 @@ func (s *Server) commandLoop() {
 	}
 }
 
-func (s *Server) stop() {
-	for i, conn := range s.conns {
-		_, err := (*conn).Write([]byte("close"))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		close(s.quitConns[i])
-		// (*conn).Close()
-		fmt.Println((*conn).RemoteAddr(), " is closed")
+func (s *Server) Stop() {
+	s.mutex.Lock()
+	for _, conn := range s.conns {
+		s.wg.Add(1)
+		go func() {
+			defer s.wg.Done()
+			fmt.Println((*conn).RemoteAddr())
+			(*conn).Close()
+		}()
 	}
+	s.mutex.Unlock()
+	close(s.quit)
 }
