a2da3a26dd776cce1af38cadf28b1e32e82d863b
ZzDangprozZ3 <lehongdang2k4@gmail.com>
2025-02-06 09:50:54 +0100

    i can open source in my pc but not discord

---
+++ elm/Model.elm
@@ -0,0 +1,8 @@
+module Model exposing(..)   
+
+
+type Command = Left Int | Right Int | Forward Int | Repeat Int (List Command)     ------Create type Command and Model to exposing
+
+
+type alias Model = 
+  { content : List Command , input : String, color : String, largeur : Int}
