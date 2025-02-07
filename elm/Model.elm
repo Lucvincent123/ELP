496209636f765aa3704e53d0f73c87a8b485b2e4
ZzDangprozZ3 <lehongdang2k4@gmail.com>
2025-02-07 15:35:57 +0100

    finished

--- elm/Model.elm
+++
@@ -1,8 +0,0 @@
-module Model exposing(..)   
-
-
-type Command = Left Int | Right Int | Forward Int | Repeat Int (List Command)     ------Create type Command and Model to exposing
-
-
-type alias Model = 
-  { content : List Command , input : String, color : String, largeur : Int}
